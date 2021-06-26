26315321babdebc0fea03350c5d0effcfb49b529
Sergey Ovsyannikov <82229027+SergeyOvs@users.noreply.github.com>
2021-06-26 17:30:53 +0300

    Add files via upload

---
+++ OOP DZ 1.1  .cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+class Numbers
+{
+public:
+	double m_basis;
+	double m_exponent;
+	
+
+	void set(double basis, double exponent)
+	{
+		m_basis = basis;
+		m_exponent = exponent;
+	}
+	
+
+	void print()
+	{
+		cout << "BASIS" << endl << m_basis << endl << "EXPONENT" <<endl << m_exponent << endl;
+		cout << "Resalt" << endl << pow(m_basis,m_exponent) << endl;
+	}
+};
+
+int main()
+{
+	Numbers n1;
+	n1.set(3.2,5.3);
+	
+	n1.print();
+	
+
+	return 0;
+}
